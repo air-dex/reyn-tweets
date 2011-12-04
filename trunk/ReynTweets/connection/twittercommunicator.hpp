0c257417e92fffeeb88062622a26a0e6886c7888
ducher.romain@gmail.com <ducher.romain@gmail.com@4338a3e6-1ccb-f1bc-79d6-db3a6126e817>
2011-12-04 23:36:10 +0000

    Fix sur les paramètres du projets + début de la mise en place du Twitter Communicator.

--- trunk/ReynTweets/connection/twittercommunicator.hpp
+++ trunk/ReynTweets/connection/twittercommunicator.hpp
@@ -1,3 +1,7 @@
+/// @file twittercommunicator.hpp
+/// @brief Class managing communication with the Twitter API
+/// @author Romain Ducher
+
 /*
 Copyright 2011 Romain Ducher
 
@@ -10,7 +14,7 @@ the Free Software Foundation, either version 3 of the License, or
 
 Reyn Tweets is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
@@ -21,17 +25,52 @@ along with Reyn Tweets.  If not, see <http://www.gnu.org/licenses/>.
 #define TWITTERCOMMUNICATOR_HPP
 
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QByteArray>
+#include <QUrl>
+#include <QMap>
 
+/// @class TwitterCommunicator
+/// @brief Class managing communication with the Twitter API.<br/>
+/// A Twitter Communicator needs the URL of the service, GET and POST arguments.
+/// It will doing the request and storing it in a QByteArray containing the
+/// response. <strong>The content of the QByteArray is not parsed.</strong>
 class TwitterCommunicator : public QObject
 {
-    Q_OBJECT
+Q_OBJECT
 
-    public:
-        explicit TwitterCommunicator(QObject *parent = 0);
+public:
+TwitterCommunicator(QObject *parent = 0);
+~TwitterCommunicator();
+QByteArray getresponseBuffer();
 
-    signals:
+signals:
 
-    public slots:
+public slots:
+
+protected:
+/// @brief Network manager
+QNetworkAccessManager * networkManager;
+
+/// @brief URL of the service
+QUrl serviceURL;
+
+/// @brief GET datas
+QMap getDatas;
+
+/// @brief POST datas
+QMap postDatas;
+
+/// @brief Resquest to send
+QNetworkRequest request;
+
+/// @brief Entity managing Twitter reply
+QNetworkReply * twitterReply;
+
+/// @brief Content of the response
+QByteArray responseBuffer;
 
 };
 

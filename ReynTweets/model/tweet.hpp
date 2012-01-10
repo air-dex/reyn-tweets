dbdcc6c3d628d2d85bb52499e577d494cabb8079
ducher.romain@gmail.com <ducher.romain@gmail.com@4338a3e6-1ccb-f1bc-79d6-db3a6126e817>
2012-01-10 03:01:53 +0000

    Attributs de la classe User + requêtes exemples

---
+++ ReynTweets/model/tweet.hpp
@@ -0,0 +1,43 @@
+/// @file tweet.hpp
+/// @brief Header of Tweet
+/// @author Romain Ducher
+
+/*
+Copyright 2012 Romain Ducher
+
+This file is part of Reyn Tweets.
+
+Reyn Tweets is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Reyn Tweets is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Reyn Tweets. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TWEET_HPP
+#define TWEET_HPP
+
+#include <QObject>
+
+/// @class Tweet
+/// @brief Model of a tweet
+class Tweet : public QObject
+{
+		Q_OBJECT
+	public:
+		explicit Tweet(QObject *parent = 0);
+
+	signals:
+
+	public slots:
+
+};
+
+#endif // TWEET_HPP

e7d9c445a9dfeb73cd94c7eb86240b0a25f99213
ducher.romain@gmail.com <ducher.romain@gmail.com@4338a3e6-1ccb-f1bc-79d6-db3a6126e817>
2012-02-08 00:53:21 +0000

    Licence dans lka documentation Doxygen

--- ReynTweets/model/reyntweetsmappable.hpp
+++ ReynTweets/model/reyntweetsmappable.hpp
@@ -1,25 +1,25 @@
 /// @file reyntweetsmappable.hpp
 /// @brief Header of ReynTweetsMappable
 /// @author Romain Ducher
-
-/*
-Copyright 2012 Romain Ducher
-
-This file is part of Reyn Tweets.
-
-Reyn Tweets is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Reyn Tweets is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Reyn Tweets. If not, see <http://www.gnu.org/licenses/>.
-*/
+///
+/// @section LICENSE
+///
+/// Copyright 2012 Romain Ducher
+///
+/// This file is part of Reyn Tweets.
+///
+/// Reyn Tweets is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Reyn Tweets is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with Reyn Tweets. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef REYNTWEETSMAPPABLE_HPP
 #define REYNTWEETSMAPPABLE_HPP
--- ReynTweets/tools/utils.cpp
+++ ReynTweets/tools/utils.cpp
@@ -3,25 +3,25 @@
 ///
 /// Revisions older than r120 are in the folder /trunk/ReynTweets/connection/.
 /// @author Romain Ducher
-
-/*
-Copyright 2011 Romain Ducher
-
-This file is part of Reyn Tweets.
-
-Reyn Tweets is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Reyn Tweets is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with Reyn Tweets.  If not, see <http://www.gnu.org/licenses/>.
-*/
+///
+/// @section LICENSE
+///
+/// Copyright 2011 Romain Ducher
+///
+/// This file is part of Reyn Tweets.
+///
+/// Reyn Tweets is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Reyn Tweets is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with Reyn Tweets. If not, see <http://www.gnu.org/licenses/>.
 
 #include <QCryptographicHash>
 #include <QJson/QObjectHelper>

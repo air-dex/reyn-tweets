4d30c1337b4e8f64ecaf97d13967b805a064bc84
ducher.romain <ducher.romain@4338a3e6-1ccb-f1bc-79d6-db3a6126e817>
2012-09-11 00:59:23 +0000

    [API 1.1] Màj des requêtes sur les utilisateurs

--- ReynTweets/connection/requests/accounts/verifycredentialsrequester.hpp
+++ ReynTweets/connection/requests/accounts/verifycredentialsrequester.hpp
@@ -29,7 +29,7 @@
 /// @class VerifyCredentialsRequester
 /// @brief Request to verify if the access tokens owned by Reyn Tweets
 /// to use a given Twitter account are the right ones.
-/// @see https://api.twitter.com/1/account/verify_credentials.json
+/// @see https://api.twitter.com/1.1/account/verify_credentials.json
 class VerifyCredentialsRequester : public TwitterRequester
 {
 		Q_OBJECT

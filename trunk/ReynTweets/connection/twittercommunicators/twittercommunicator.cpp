ccb865fccc8eb0f99638ecbdfac72e04b33bd842
ducher.romain <ducher.romain@4338a3e6-1ccb-f1bc-79d6-db3a6126e817>
2013-08-20 00:58:39 +0000

    Bugfix sur la conf' utilisateur (mauvais fichier de conf' vide)

--- trunk/ReynTweets/connection/twittercommunicators/twittercommunicator.cpp
+++ trunk/ReynTweets/connection/twittercommunicators/twittercommunicator.cpp
@@ -162,7 +162,7 @@ void TwitterCommunicator::endRequest(QNetworkReply * response) {
 	response->deleteLater();
 
 	// responseBuffer (for debug purposes)
-	/*
+	//*
 	qDebug("responseBuffer :");
 	qDebug(responseBuffer.data());
 	qDebug("\n");

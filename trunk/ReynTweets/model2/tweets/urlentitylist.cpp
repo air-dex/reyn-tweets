b15717392c7e5450965f75db6e1aaef3673ea754
ducher.romain <ducher.romain@4338a3e6-1ccb-f1bc-79d6-db3a6126e817>
2013-08-20 01:02:04 +0000

    [Refonte Qt5] Listes de Tweet Entities

---
+++ trunk/ReynTweets/model2/tweets/urlentitylist.cpp
@@ -0,0 +1,61 @@
+/// @file urlentitylist.cpp
+/// @brief Implementation of URLEntityList
+/// @author Romain Ducher
+///
+/// @section LICENSE
+///
+/// Copyright 2013 Romain Ducher
+///
+/// This file is part of Reyn Tweets.
+///
+/// Reyn Tweets is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Reyn Tweets is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with Reyn Tweets. If not, see <http://www.gnu.org/licenses/>.
+
+#include "urlentitylist.hpp"
+
+// Constructor
+URLEntityList2::URLEntityList2() :
+	JsonArray<URLEntity2>()
+{}
+
+// Destructor
+URLEntityList2::~URLEntityList2() {}
+
+// Copy constructor
+URLEntityList2::URLEntityList2(const URLEntityList2 & list) :
+	JsonArray<URLEntity>()
+{
+	recopie(list);
+}
+
+// Affrection operator
+const URLEntityList2 & URLEntityList2::operator=(const URLEntityList2 & list) {
+	recopie(list);
+	return *this;
+}
+
+// Serialization initialization
+void URLEntityList2::initSystem() {
+	qRegisterMetaTypeStreamOperators<URLEntityList2>("URLEntityList2");
+	qMetaTypeId<URLEntityList2>();
+}
+
+// Output stream operator for serialization
+QDataStream & operator<<(QDataStream & out, const URLEntityList2 & list) {
+	return jsonStreamingOut<URLEntity2>(out, list);
+}
+
+// Input stream operator for serialization
+QDataStream & operator>>(QDataStream & in, URLEntityList2 & list) {
+	return jsonStreamingIn<URLEntity2>(in, list);
+}
---
+++ trunk/ReynTweets/model2/tweets/usermentionlist.hpp
@@ -0,0 +1,97 @@
+/// @file usermentionlist.hpp
+/// @brief Header of UserMentionList
+/// @author Romain Ducher
+///
+/// @section LICENSE
+///
+/// Copyright 2013 Romain Ducher
+///
+/// This file is part of Reyn Tweets.
+///
+/// Reyn Tweets is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Lesser General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// Reyn Tweets is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+/// GNU Lesser General Public License for more details.
+///
+/// You should have received a copy of the GNU Lesser General Public License
+/// along with Reyn Tweets. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef USERMENTIONLIST2_HPP
+#define USERMENTIONLIST2_HPP
+
+#include <QVariant>
+#include "usermention.hpp"
+#include "../json/jsonarray.tpp"
+
+/// @class UserMentionList
+/// @brief List of user mentions
+class UserMentionList2 : public JsonArray<UserMention2>
+{
+	public:
+		/// @fn UserMentionList();
+		/// @brief Constructor
+		UserMentionList2();
+
+		/// @fn virtual ~UserMentionList();
+		/// @brief Destructor
+		virtual ~UserMentionList2();
+
+		/// @fn UserMentionList(const UserMentionList & list);
+		/// @brief Copy constructor
+		/// @param list UserMentionList to copy
+		UserMentionList2(const UserMentionList2 & list);
+
+		/// @fn const UserMentionList & operator=(const UserMentionList & list);
+		/// @brief Affrection operator
+		/// @param list UserMentionList to affect
+		const UserMentionList2 & operator=(const UserMentionList2 & list);
+
+		/// @fn static void initSystem();
+		/// @brief Serialization declaration
+		static void initSystem();
+
+	protected:
+		// Friends serialization operators
+
+		/// @fn friend QDataStream & operator<<(QDataStream & out,
+		///										const UserMentionList & list);
+		/// @brief Output stream operator for serialization
+		/// @param out The output stream
+		/// @param list Object to put in the stream
+		/// @return The stream with the object
+		friend QDataStream & operator<<(QDataStream & out,
+										const UserMentionList2 & list);
+
+		/// @fn friend QDataStream & operator>>(QDataStream & in,
+		///										UserMentionList & list);
+		/// @brief Input stream operator for serialization
+		/// @param in The input stream
+		/// @param list Object to put in the stream
+		/// @return The stream with the object
+		friend QDataStream & operator>>(QDataStream & in,
+										UserMentionList2 & list);
+};
+
+// Serialization of UserMentionList
+Q_DECLARE_METATYPE(UserMentionList2)
+
+/// @fn QDataStream & operator<<(QDataStream & out, const UserMentionList & list);
+/// @brief Output stream operator for serialization
+/// @param out The output stream
+/// @param list Object to put in the stream
+/// @return The stream with the object
+QDataStream & operator<<(QDataStream & out, const UserMentionList2 & list);
+
+/// @fn QDataStream & operator>>(QDataStream & in, UserMentionList & list);
+/// @brief Input stream operator for serialization
+/// @param in The input stream
+/// @param list Object to put in the stream
+/// @return The stream with the object
+QDataStream & operator>>(QDataStream & in, UserMentionList2 & list);
+
+#endif // USERMENTIONLIST_HPP
